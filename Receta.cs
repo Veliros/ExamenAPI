d337af9a49f3aa2bd41402da8dbdda2019671e18
Veliros <37800192+Veliros@users.noreply.github.com>
2019-02-01 15:49:40 +0100

    Controller, repository y clase.

---
+++ Receta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace examen.Models
+{
+    public class Receta
+    {
+        // Variables:
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string comentarios { get; set; }
+        public int dificultad { get; set; }
+
+        // Constructor:
+        public Receta(int id, string titulo, string comentarios, int dificultad)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.comentarios = comentarios;
+            this.dificultad = dificultad;
+        }
+    }
+}
---
+++ RecetasController.cs
@@ -0,0 +1,47 @@
+﻿using examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace examen.Controllers
+{
+    public class RecetasController : ApiController
+    {
+        // GET: api/Recetas
+        public IEnumerable<Receta> Get()
+        {
+            // recuperación de todas las recetas:
+            var repo = new Recetasrepository();
+            List<Receta> recetas = repo.Retrieve();
+
+            return recetas;
+        }
+
+        // GET: api/Recetas/5
+        public Receta Get(int id)
+        {
+            // Llamada al método de recuperación por id:
+            var repo = new Recetasrepository();
+
+            return repo.RetrieveId(id);
+        }
+
+        // POST: api/Recetas
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Recetas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Recetas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
